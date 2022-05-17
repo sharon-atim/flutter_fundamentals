f78d3017e3e5b37c972bb56162d9b6b1f6ec531a
Sharon Atim <venusatim@gmail.com>
2022-05-17 13:11:34 +0100

    StreamBuilder example added

--- lib/Streams/streams_streamBuilder.dart
+++ lib/Streams/streams_streamBuilder.dart
@@ -1,20 +1,5 @@
-import 'dart:async';
 import 'package:flutter/material.dart';
-
-void main() => runApp(StreamBuilderApp());
-
-class StreamBuilderApp extends StatelessWidget {
-  const StreamBuilderApp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-        title: 'StreamBuilder Demo',
-        theme: ThemeData(
-          primaryColor: Colors.purpleAccent,
-        ));
-  }
-}
+import 'dart:async';
 
 class CounterApp extends StatefulWidget {
   const CounterApp({Key? key}) : super(key: key);
@@ -24,12 +9,60 @@ class CounterApp extends StatefulWidget {
 }
 
 class _CounterAppState extends State<CounterApp> {
-  //Creates an instance of the stream controller class
+  // Creates an instance of the stream controller class
   final StreamController _controller = StreamController();
-  final int _counter = 0;
+  // Set initial counter value to 10 seconds;
+  int _counter = 10;
+
+// StartTimer method
+
+  void startTimer() async {
+// Timer method runs every second
+    Timer.periodic(const Duration(seconds: 1), (timer) {
+      _counter--;
+      // Add event/data to stream controller using sink.
+      _controller.sink.add(_counter);
+
+      // Will stop Count Down timer when _counter valie is 0
+      if (_counter <= 0) {
+        timer.cancel();
+        _controller.close();
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    // Destroy the stream controllwer when user exits the app
+    _controller.close();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            StreamBuilder(
+                initialData: _counter,
+                stream: _controller.stream,
+                builder: (context, snapshot) {
+                  return Text('${snapshot.data}');
+                }),
+            const SizedBox(
+              height: 20,
+            ),
+            ElevatedButton(
+                onPressed: () {
+                  // Start the timer
+                  startTimer();
+                },
+                child: const Text('Start Count Down'))
+          ],
+        ),
+      ),
+    );
   }
 }
